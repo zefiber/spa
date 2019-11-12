bc89839ebeb2fea356aeefbb03e4d1ac9e054ee5
Ze Wang <zwang@exiger.com>
2019-11-12 11:47:45 -0500

    Initial commit for the nodejs front-end

---
+++ material-kit-react-master/src/views/ProfilePage/ProfilePage.js
@@ -0,0 +1,159 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+// @material-ui/icons
+import Camera from "@material-ui/icons/Camera";
+import Palette from "@material-ui/icons/Palette";
+import Favorite from "@material-ui/icons/Favorite";
+// core components
+import Header from "components/Header/Header.js";
+import Footer from "components/Footer/Footer.js";
+import Button from "components/CustomButtons/Button.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import AdvancedGridList from "components/Grid/AdvancedGridLine.js";
+import GridItem from "components/Grid/GridItem.js";
+import NavPills from "components/NavPills/NavPills.js";
+import Parallax from "components/Parallax/Parallax.js";
+
+import profile from "assets/img/faces/starwar.jpeg";
+
+import studio1 from "assets/img/examples/studio-1.jpg";
+import studio2 from "assets/img/examples/studio-2.jpg";
+import studio3 from "assets/img/examples/studio-3.jpg";
+import studio4 from "assets/img/examples/studio-4.jpg";
+import studio5 from "assets/img/examples/studio-5.jpg";
+import work1 from "assets/img/examples/olu-eletu.jpg";
+import work2 from "assets/img/examples/clem-onojeghuo.jpg";
+import work3 from "assets/img/examples/cynthia-del-rio.jpg";
+import work4 from "assets/img/examples/mariya-georgieva.jpg";
+import work5 from "assets/img/examples/clem-onojegaw.jpg";
+
+import styles from "assets/jss/material-kit-react/views/profilePage.js";
+
+
+
+
+
+
+
+
+const useStyles = makeStyles(styles);
+
+
+export default function ProfilePage(props) {
+
+  const classes = useStyles();
+  const { ...rest } = props;
+  const imageClasses = classNames(
+    classes.imgRaised,
+    classes.imgRoundedCircle,
+    classes.imgFluid
+  );
+  const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
+  return (
+    <div>
+      <Header
+        color="transparent"
+        brand="Ze's Star War Machete"
+        fixed
+        changeColorOnScroll={{
+          height: 200,
+          color: "white"
+        }}
+        {...rest}
+      />
+      <Parallax small filter image={require("assets/img/profile-bg.jpg")} />
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <div>
+          <div className={classes.container}>
+            <GridContainer justify="center">
+              <GridItem xs={12} sm={12} md={6}>
+                <div className={classes.profile}>
+                  <div>
+                    <img src={profile} alt="..." className={imageClasses} />
+                  </div>
+                  <div className={classes.name}>
+                    <h3 className={classes.title}>May the Force be with you.</h3>
+                  </div>
+                </div>
+              </GridItem>
+            </GridContainer>
+            <div className={classes.description}>
+              <p>
+              Currently there does not seem to be any good online resource that displays the Star Wars movies in
+              story order (Episode I-VII) as opposed to chronologically (1978-2015). Worse, they are not optionally
+              presented in the obviously superior “Machete” order, which, as everyone knows, is the superior order
+              to watch them.{" "}
+              </p>
+            </div>
+            <GridContainer justify="center">
+              <GridItem xs={12} sm={12} md={8} className={classes.navWrapper}>
+                <NavPills
+                  alignCenter
+                  color="primary"
+                  tabs={[
+                    {
+                      tabButton: "Episode",
+                      tabIcon: Camera,
+                      tabContent: (
+                        <AdvancedGridList></AdvancedGridList>
+                      )
+                    },
+                    {
+                      tabButton: "Release",
+                      tabIcon: Palette,
+                      tabContent: (
+                        <AdvancedGridList></AdvancedGridList>
+                      )
+                    },
+                    {
+                      tabButton: "Favorite",
+                      tabIcon: Favorite,
+                      tabContent: (
+                        <GridContainer justify="center">
+                          <GridItem xs={12} sm={12} md={4}>
+                            <img
+                              alt="..."
+                              src={work4}
+                              className={navImageClasses}
+                            />
+                            <img
+                              alt="..."
+                              src={studio3}
+                              className={navImageClasses}
+                            />
+                          </GridItem>
+                          <GridItem xs={12} sm={12} md={4}>
+                            <img
+                              alt="..."
+                              src={work2}
+                              className={navImageClasses}
+                            />
+                            <img
+                              alt="..."
+                              src={work1}
+                              className={navImageClasses}
+                            />
+                            <img
+                              alt="..."
+                              src={studio1}
+                              className={navImageClasses}
+                            />
+                          </GridItem>
+                        </GridContainer>
+                      )
+                    }
+                  ]}
+                />
+              </GridItem>
+            </GridContainer>
+          </div>
+          
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
